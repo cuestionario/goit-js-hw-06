befc727c3499d1238db4c5f3fd6e0bfa3df400d8
cuestionario <garcia.rafo@gmail.com>
2023-09-26 08:21:25 -0500

    ejercicios 6 y 7
    6. Validacion de caracteres en el formulario
    7. cambios del tamaño de la fuente con control deslizante

--- js/task-06.js
+++ js/task-06.js
@@ -1,5 +1,10 @@
 const validationInput = document.getElementById('validation-input');
 
+//cuando se cambia el enfoque del input se ejecuta la siguiente validación
+//que se hayan ingresado 6 caracteres, por tanto, el codigo es valido y se 
+//resalta el marco en color verde, eliminando la opción de invalido. Lo contrario
+//también ocurre.
+
 validationInput.addEventListener('blur', () => {
   const inputValue = validationInput.value;
   const expectedLength = validationInput.getAttribute('data-length');
