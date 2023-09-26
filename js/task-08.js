5ea0862f879c3a8aeba73fa35792ec324d9f1c61
cuestionario <garcia.rafo@gmail.com>
2023-09-26 09:03:43 -0500

    Tarea 9
    Cambiar color de fondo

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", function (event) {
 
+    event.preventDefault();   // Evitar que la página se recargue
+    const emailInput = this.elements.email;   // entrada de correo
+    const passwordInput = this.elements.password;   // entrada de contraseña
+
+    const emailValue = emailInput.value;
+    const passwordValue = passwordInput.value;
+
+    if (emailValue === "" || passwordValue === "") {
+        alert("Todos los campos deben ser completados.");
+    } else {
+        const formatoDatos = {
+            email: emailValue,
+            password: passwordValue,
+        };
+        console.log(formatoDatos);
+        this.reset();
+    }
+});
